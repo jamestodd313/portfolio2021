526c734ea779aee661077e2bb2695ae7d036b0fd
jamestodd313 <jamestodd313@gmail.com>
2021-02-01 14:48:59 -0600

    fonts

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Bungee+Hairline&family=Lacquer&family=Montserrat&family=Staatliches&display=swap" rel="preload" as="style"/>
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Bungee+Hairline&family=Lacquer&family=Montserrat&family=Staatliches&display=swap" as="style"/>
       </Head>
       <Component {...pageProps} />
     </>
