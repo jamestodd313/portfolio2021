89ee6996b8aac198699d4b81788a27bb130aa0af
jamestodd313 <jamestodd313@gmail.com>
2021-02-16 11:12:44 -0600

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,17 @@ function MyApp({ Component, pageProps }) {
   return(
     <>
       <Head>
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-P3WBVCMECD"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+          gtag('config', 'G-P3WBVCMECD');
+        </script>
+
+
+
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Bungee+Hairline&family=Lacquer&family=Montserrat&family=Staatliches&display=swap" rel="stylesheet"/>
         {/* <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet"/> */}
