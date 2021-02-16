7f3b1338ac8f099e2d9c8ce40d2bccb8fa858498
jamestodd313 <jamestodd313@gmail.com>
2021-02-16 12:45:36 -0600

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,48 @@
+// import Head from 'next/head'
+// import '../styles/main.css'
+// function MyApp({ Component, pageProps }) {
+  // return(
+  //   <>
+  //     <Head>
+  //       <link rel="preconnect" href="https://fonts.gstatic.com"/>
+  //       <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Bungee+Hairline&family=Lacquer&family=Montserrat&family=Staatliches&display=swap" rel="stylesheet"/>
+  //       {/* <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet"/> */}
+  //     </Head>
+  //     <Component {...pageProps} />
+  //   </>
+  // ) 
+// }
+
+// export default MyApp
+
+
+
 import Head from 'next/head'
 import '../styles/main.css'
-function MyApp({ Component, pageProps }) {
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return(
     <>
       <Head>
-
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-P3WBVCMECD"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-          gtag('config', 'G-P3WBVCMECD');
-        </script>
-
-
-
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Bungee+Hairline&family=Lacquer&family=Montserrat&family=Staatliches&display=swap" rel="stylesheet"/>
-        {/* <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet"/> */}
       </Head>
       <Component {...pageProps} />
     </>
   ) 
 }
 
-export default MyApp
+export default App
