4d996e29906f4213c957f029a2a78b461c1bb0ee
jamestodd313 <jamestodd313@gmail.com>
2021-03-19 09:38:44 -0500

    updated packages, bug fixes

--- components/projects/ProjectImage.jsx
+++ components/projects/ProjectImage.jsx
@@ -18,7 +18,7 @@ export const ProjectImage = ({project, setSelectedProject, isExpanded}) => {
     return (
         <a tabIndex="0" className={`project-thumbnail-wrapper ${isExpanded ? 'expanded' : ''}`} id={project.slug} onFocus={e=> handleHover(e)} onMouseOver={e=> handleHover(e)} onMouseLeave={unHover} onClick={e=> handleClick(e)}>
             <img src={project.coverImage} className="project-thumbnail" rel="preload" alt={project.title}/>
-            <span className="project-thumbnail-title">{project.title}</span>
+            {/* <span className="project-thumbnail-title">{project.title}</span> */}
         </a>
     )
 }
