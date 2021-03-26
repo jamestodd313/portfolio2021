d34d453950f1898240adc68106d0c897f69d0639
jamestodd313 <jamestodd313@gmail.com>
2021-03-26 14:49:21 -0500

    api test

--- pages/work/[project].js
+++ pages/work/[project].js
@@ -116,20 +116,20 @@ export default function project ({project}){
 }
 
 
-export const getStaticProps = async(ctx)=> {
+export const getServerSideProps = async(ctx)=> {
     const projectCall = await fetch(`https://jamestodd.dev/api/_v1/interface/projects/${ctx.params.project}`)
     // const projectCall = await fetch(`http://localhost:3000/api/_v1/interface/projects/${ctx.query.project}`)
     const projectData = await projectCall.json()
     return {props: {project: projectData}}
 }
 
-export const getStaticPaths = async()=> {
-    const projectsCall = await fetch('https://jamestodd.dev/api/_v1/interface/projects')
-    // const projectsCall = await fetch('http://localhost:3000/api/_v1/interface/projects')
-    const projectsData = await projectsCall.json()
+// export const getStaticPaths = async()=> {
+//     const projectsCall = await fetch('https://jamestodd.dev/api/_v1/interface/projects')
+//     // const projectsCall = await fetch('http://localhost:3000/api/_v1/interface/projects')
+//     const projectsData = await projectsCall.json()
 
-    let slugs = projectsData.map(project=> project.slug)
-    let paths = slugs.map(project=> ({params: {project: project}}))
+//     let slugs = projectsData.map(project=> project.slug)
+//     let paths = slugs.map(project=> ({params: {project: project}}))
  
-    return {paths, fallback: false}
-}
+//     return {paths, fallback: false}
+// }
